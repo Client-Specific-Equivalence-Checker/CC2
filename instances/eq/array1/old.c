90ed123ce525b7f08eead21a126d0e703d49a772
Shuolin Wang <shuolin98@gmail.com>
2021-10-26 23:20:47 -0400

    Add two array instances

---
+++ instances/eq/array1/old.c
@@ -0,0 +1,17 @@
+int lib(int *array, int n) {
+    for (int i = 0; i < n; ++i) {
+        if (i % 2 == 1) {
+            array[i] += 1;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int client(int k) {
+    int array[] = {23, -121, 12, 3, -13, 0, 335, -22};
+    return lib(array, sizeof(array) / sizeof(array[0]));
+}
---
+++ instances/eq/array2/old.c
@@ -0,0 +1,25 @@
+#include <malloc.h>
+
+int lib(int array1[], int array2[], int n) {
+    int *array = array1;
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int client(int n) {
+    int *array1 = malloc(n * sizeof(int));
+    int *array2 = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; ++i) {
+        array1[i] = i;
+        array2[i] = n - i - 1;
+    }
+
+    int sum = lib(array1, array2, n);
+    free(array1);
+    free(array2);
+    return sum;
+}
